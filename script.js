e2a8a94327864a9be8d5ddd4bfc7d8b416801fe9
Prashant Pal <palprashant465@gmail.com>
2025-03-28 18:46:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-//your JS code here. If required.
+
+const updateTimer = ()=>{
+	let ele= document.getElementById('timer');
+	let now = new Date();
+	ele.textContent = now.toLocaleString();
+}
+
+setInterval(updateTimer,1000);
+
+updateTimer();
+
